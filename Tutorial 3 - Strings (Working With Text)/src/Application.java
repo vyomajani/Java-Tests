3624736eae944af8765690142c33cf09cff6e6ef
vyomajani <vyomajani@gmail.com>
2020-05-23 13:23:18 -0400

    Learned strings

---
+++ Tutorial 3 - Strings (Working With Text)/src/Application.java
@@ -0,0 +1,8 @@
+
+public class Application {
+	public static void main(String[] args) {
+		short myShort = 7;
+		System.out.println("Heyyo!" + " " + "It's" + " " + "me!" + " " + "Vyoma!");
+		System.out.println("This is my favorite number:" + myShort);
+}
+}
