b31550062e5556fdb4aaece422769a5e6203a127
vyomajani <vyomajani@gmail.com>
2020-05-23 13:24:09 -0400

    Learned while loops

---
+++ Tutorial 4 - While Loops/src/Application.java
@@ -0,0 +1,13 @@
+public class Application {
+	public static void main(String[] args) {
+		
+		int value = 1;
+		
+		while(value < 10)
+		{		
+			System.out.println(value + ". " + "List");
+			
+			value = value + 1;
+		}
+	}
+}
