a7dd45ffd15b6b991fcc224fa13bade22f6fdb4e
vyomajani <vyomajani@gmail.com>
2020-05-23 13:25:44 -0400

    Learned for loops

---
+++ Tutorial 5 - For Loops/src/Application.java
@@ -0,0 +1,9 @@
+public class Application {
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 4; i++){
+			System.out.println(i + ". Hey there");
+			System.out.printf("  This number is %d\n", i);
+		}
+	}
+}
