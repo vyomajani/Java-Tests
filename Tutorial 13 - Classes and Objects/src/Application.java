cbb38dd500e4c22f122337966ed9f1e3a8a6319f
vyomajani <vyomajani@gmail.com>
2020-05-24 20:33:33 -0400

    Intro to OOP in Java

---
+++ Tutorial 13 - Classes and Objects/src/Application.java
@@ -0,0 +1,19 @@
+
+class Person {
+	String name;
+	int age;
+}
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Person p1 = new Person();
+		p1.name = "Jim";
+		p1.age = 38;
+		
+		System.out.println(p1.age);
+		
+	}
+
+}
