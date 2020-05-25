5f28ca4fd9f69fece2da1b3b53c1ba77c6754428
vyomajani <vyomajani@gmail.com>
2020-05-24 20:32:08 -0400

    For each with an array of Strings

---
+++ Tutorial 11 - Arrays of Strings/src/Application.java
@@ -0,0 +1,23 @@
+public class Application {
+
+	public static void main(String[] args) {
+		String[] eyyy = new String[3];
+
+		eyyy[0] = "How's ";
+		eyyy[1] = "it ";
+		eyyy[2] = "going?";
+
+		System.out.println(eyyy[0] + eyyy[1] + eyyy[2]);
+
+		String[] bruh = { "I'm ", "tired." };
+
+		for (String fruit : bruh) {
+			System.out.println(fruit);
+		}
+		
+		String[] file = new String [2];
+		
+		System.out.println(file[1]);
+	}
+
+}
